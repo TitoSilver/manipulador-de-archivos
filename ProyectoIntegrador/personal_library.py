e2d2f882a3ca35df5a48270816a6da884d2488b2
TitoSilver <diegofornes2@gmail.com>
2021-06-14 10:05:32 -0300

    se termino de pulir el acceso al script mediante comando

--- ProyectoIntegrador/personal_library.py
+++ ProyectoIntegrador/personal_library.py
@@ -12,9 +12,6 @@ class Node_file:
         
 list_file= LinkedList()
 def create (path):
-    print("llega aca1")
-    path= r"D:\Back up\FACULTAD\Algoritmos y Estructura de Datos II\codigo\ProyectoIntegrador\test_files"
-    print("llega aca2")
     with os.scandir(path) as it:
         listOfWords=LinkedList()
         for file in it:
@@ -31,7 +28,9 @@ def create (path):
                         if newList:
                             listOfWords= concatenateList(newList,listOfWords)
                     
+                    #agrega a la lista final el nodo con (nombre del archivo,hashTable del archivo)
                     #add(list_file,Node_file(fileNameOnly,listOfWords))
+                    
                     print("cant Palabras: ",count_words)       
                     currentNode=listOfWords.head
                     while currentNode:
@@ -45,86 +44,33 @@ if __name__ == '__main__':
     listArguments= sys.argv
     print(listArguments)
     if len(listArguments)!= 3:
-        print("Por favor ingrese una instrucción valida")
-        print (os.getcwd())
-        
-    elif listArguments[1]== "--create" :
-        create("hola")
+        if len(listArguments) > 3:
+            path=listArguments[2]
+            for idx in range(3,len(listArguments)):
+                path +=" " +listArguments[idx]
+                
+            if os.path.exists(path):
+                print("el path que pasa como parametro es: ",path)
+                create(path)
+        else:
+            #Pasa un comando en el que faltan argumentos
+            print("Por favor ingrese una instrucción valida")
+            
+    elif listArguments[1]== "--create" or listArguments[1]== "-create":
+        if os.path.exists(listArguments[2]):
+            print("el path que pasa como parametro es: ",listArguments[2])
+            if os.path.exists(listArguments[2]):
+                create(listArguments[2])
     
-    elif listArguments[1]== "--search":
+    elif listArguments[1]== "--search" or listArguments[1]== "-search":
         print("uwu")
     else:
+        #Pasa un comando en el que existen todos los argumentos, pero la función que pasa no es correcta
         print("Por favor ingrese una instrucción valida")
 
+    #PATH DE PRUEBA
+    # py personal_library.py --create D:\Back up\FACULTAD\Algoritmos y Estructura de Datos II\codigo\proyecto-grupo4\ProyectoIntegrador\test_files
+    # py personal_library.py -create D:\Back up\FACULTAD\Algoritmos y Estructura de Datos II\codigo\proyecto-grupo4\ProyectoIntegrador\test_files
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-"""
-list_file= LinkedList()
-
-class Node_file:
-    def __init__(self,name_file,hash_of_words):
-        self.nameFile: name_file
-        self.hash_of_words: hash_of_words
-
-path= r"D:\Back up\FACULTAD\Algoritmos y Estructura de Datos II\codigo\ProyectoIntegrador\test_files"
-
-arr_files = os.listdir(path)
-
-print(arr_files)
-        
-list_prev= LinkedList()
-
-for file in arr_files:
-    uwu=file
-    if ".txt" in file:
-        with open(path+"\\"+file,"r") as file:
-            lines= file.readlines()
-            count_words= 0
-            count_line= 0
-            for line in lines:
-                count_words_in_line, list_of_words_in_line= travel_line(line)
-                count_words += count_words_in_line
-    add(list_prev,ContainerWords(uwu,list_of_words_in_line))
-
-currentNode= list_prev.head
-while currentNode:
-    print("Dentro del archivo: ",currentNode.value.nameFile)
-    intoCurrent=currentNode.value.list_of_line_words.head
-    while intoCurrent:
-        print(intoCurrent.value)
-        intoCurrent= intoCurrent.nextNode
-    
-    currentNode=currentNode.nextNode
-
-"""
-
-    
-"""
-with open(path+"\\"+file,"r") as path_file:
-    size_hash= sum(1 for _ in path_file if _.rstrip("\n"))
-    
-    hash_of_words= Array(size_hash,Dictionary())
-    print("file: {0} y cant_lineas: {1}".format(file,size_hash))
-
-with open(path+"\\"+file,"r") as file:
-    lines= file.readlines()
-    
-    for line in lines:
-
-"""
-
+    # py personal_library.py -create C:\Users\ULTRABYTES\Desktop\test_files
+    # py personal_library.py --create C:\Users\ULTRABYTES\Desktop\test_files
